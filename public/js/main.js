254fce3d60a14103f0e0d130a8903b533f655182
hankusa1709 <handyk1709@gmail.com>
2022-10-16 20:40:54 +0700

    used gallery lightbox plugin

--- public/js/main.js
+++ public/js/main.js
@@ -5,7 +5,18 @@
    * Easy selector helper function
    */
 
-   var swiper = new Swiper(".ads-slider", {
+  /**
+   * Initiate glightbox 
+   */
+   const glightbox = GLightbox({
+    selector: '.glightbox'
+  });
+
+  const galleryLightbox = GLightbox({
+    selector: '.gallery-lightbox'
+  });
+  
+   new Swiper(".ads-slider", {
     slidesPerView: 1,
     loop: true,
     autoplay: {
@@ -13,6 +24,32 @@
       disableOnInteraction: false,
     },
    });
+
+   new Swiper('.testimonials-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      }
+    }
+  });
 
   const select = (el, all = false) => {
     el = el.trim()
